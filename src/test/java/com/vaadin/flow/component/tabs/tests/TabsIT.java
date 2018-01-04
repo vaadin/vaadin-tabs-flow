1343ed950fe50d0a5d59440fdfd6ff44b2dee71b
Gilberto Torrezan <gilberto@vaadin.com>
2018-01-04 10:07:32 +0200

    Change the demo to use Component::setVisible instead of CSS

--- src/test/java/com/vaadin/flow/component/tabs/tests/TabsIT.java
+++ src/test/java/com/vaadin/flow/component/tabs/tests/TabsIT.java
@@ -16,15 +16,16 @@
 
 package com.vaadin.flow.component.tabs.tests;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
-
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 import com.vaadin.flow.component.tabs.demo.TabsView;
 import com.vaadin.flow.demo.ComponentDemoTest;
-import com.vaadin.testbench.By;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 /**
  * Integration tests for the {@link TabsView}.
  *
