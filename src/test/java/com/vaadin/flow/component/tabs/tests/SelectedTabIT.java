2226f99d3d093c3c3bb83c7ea931ec13104aec33
Pekka Maanpää <pekkamaa@vaadin.com>
2018-08-29 16:21:56 +0300

    Fire selection events from server, keep selected tab on add/remove (#68)
    * Fire selection events from server, keep selected tab on add/remove
    
    - SelectedChangeEvent is no longer annotated with DomEvent, but a PropertyChangeListener is used internally
    - The event is fired synchronously from server-side when the selection happens in server-side
    - Event.isFromClient() returns correct value
    - Adding and removing tabs changes the selected index to not change the selected tab
    - If the selected tab is removed, the selection is changed and event is fired
    
    * Fix removing non-child tab
    
    * Add test for appending child with Element API
    
    * Add javadocs

--- src/test/java/com/vaadin/flow/component/tabs/tests/SelectedTabIT.java
+++ src/test/java/com/vaadin/flow/component/tabs/tests/SelectedTabIT.java
@@ -18,6 +18,7 @@ package com.vaadin.flow.component.tabs.tests;
 import java.util.List;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -29,10 +30,13 @@ import com.vaadin.testbench.TestBenchElement;
 @TestPath("selected-tab")
 public class SelectedTabIT extends AbstractComponentIT {
 
+    @Before
+    public void init() {
+        open();
+    }
+
     @Test
     public void verifyTabIsSelected() {
-        open();
-
         findElement(By.id("second")).click();
         findElement(By.id("show-selection")).click();
 
@@ -51,9 +55,21 @@ public class SelectedTabIT extends AbstractComponentIT {
     }
 
     @Test
-    public void selectDisabledTab_selectionIsResetAndRedisabled() {
-        open();
+    public void removeSelectedTabFromServer_changeEventFromServer() {
+        findElement(By.id("delete")).click();
+        assertSelectionEvent(1, "bar server");
+    }
 
+    @Test
+    public void selectSecondTab_eventFromClient_deleteFirstTab_noEvent() {
+        findElement(By.id("second")).click();
+        assertSelectionEvent(1, "bar client");
+        findElement(By.id("delete-first")).click();
+        assertSelectionEvent(1, "bar client");
+    }
+
+    @Test
+    public void selectDisabledTab_noSelectionEvent_tabIsRedisabled() {
         List<TestBenchElement> tabs = $("vaadin-tabs").first().$("vaadin-tab")
                 .all();
         TestBenchElement lastTab = tabs.get(tabs.size() - 1);
@@ -63,25 +79,35 @@ public class SelectedTabIT extends AbstractComponentIT {
 
         lastTab.click();
 
-        WebElement selectionEvent = findElement(By.id("selection-event"));
-        Assert.assertEquals("foo", selectionEvent.getText());
+        assertSelectionEvent(0, null);
 
         Assert.assertEquals(Boolean.TRUE.toString(),
                 lastTab.getAttribute("disabled"));
     }
 
-    @Test
-    public void selectionEventOnItemsChange() {
-        open();
+    @Test // https://github.com/vaadin/vaadin-tabs-flow/issues/69
+    public void addTabAsFirstWithElementAPI_selectionIsChanged_eventFromClient() {
+        findElement(By.id("add-first-with-element-api")).click();
+        assertSelectionEvent(1, "asdf client");
+    }
 
-        findElement(By.id("delete")).click();
+    private void assertSelectionEvent(int amountOfEvents,
+            String expectedLatestMessage) {
+        List<WebElement> selectionEventMessages = findElements(
+                By.className("selection-event"));
 
-        WebElement selectionEvent = findElement(By.id("selection-event"));
+        Assert.assertEquals(
+                "Unexpected amount of selection events have been fired",
+                amountOfEvents, selectionEventMessages.size());
 
-        Assert.assertEquals("bar", selectionEvent.getText());
+        if (amountOfEvents == 0) {
+            return;
+        }
 
-        findElement(By.id("add")).click();
+        WebElement lastMessage = selectionEventMessages
+                .get(selectionEventMessages.size() - 1);
 
-        Assert.assertEquals("baz", selectionEvent.getText());
+        Assert.assertEquals("Unexpected message for the latest selection event",
+                expectedLatestMessage, lastMessage.getText());
     }
 }
